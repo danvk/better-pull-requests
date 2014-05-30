533c9df9e6a26c74a396a5d7dfa96b31f7398efe
Dan Vanderkam <danvdk@gmail.com>
2014-05-30 15:30:13 -0400

    list pull requests in app

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
+import github
 from flask import Flask
 app = Flask(__name__)
 
+@app.route("/pulls/<user>/<repo>")
+def pulls(user, repo):
+    pull_requests = github.get_pull_requests(user, repo)
+    def pr_fmt(pr):
+        return "#%s %s (%s)" % (pr['number'], pr['title'], pr['user.login'])
+
+    return "Pull requests:<br/>%s" % '<br/>'.join([pr_fmt(p) for p in pull_requests])
+
 @app.route("/")
 def hello():
-    return "Hello Galaxy!"
+    return "Hello World!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
