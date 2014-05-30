64fd24d17785bf6645541ab0852a5bd003db7e2b
Dan Vanderkam <danvdk@gmail.com>
2014-05-30 15:24:22 -0400

    github module, simple flask

---
+++ README.md
@@ -0,0 +1,5 @@
+== Setting Up
+
+virtualenv env                   # make a new virtualenv named 'env'.
+source env/bin/activate          # activate the env (local pip, python).
+pip install -r requirements.txt  # install the requirements in the env.
---
+++ github.py
@@ -0,0 +1,61 @@
+import sys
+
+import requests
+
+
+def extract_path(json, path):
+    parts = path.split('.')
+    try:
+        for part in parts:
+            json = json[part]
+        return json
+    except KeyError:
+        return None
+
+
+def get_pull_requests(user, repo):
+    url = 'https://api.github.com/repos/%s/%s/pulls' % (user, repo)
+    r = requests.get(url)
+    if not r.ok:
+        # TODO(danvk): print error
+        sys.stderr.write('Request for %s failed.\n' % url)
+        return None
+
+    # See https://developer.github.com/v3/pulls/
+    pull_requests = r.json()
+    paths = ['number',
+             'state',
+             'title',
+             'body',
+             'created_at',  # ISO
+             'modified_at',
+             'user.login',  # sender of the pull request
+             'head.label',  # e.g. 'kylebaggott:master'
+             'head.ref',  # e.g. 'master'
+             'head.sha',  # sha for ref
+             'head.repo.full_name',  # e.g. kylebaggott/dygraphs
+             'head.repo.clone_url',  # repo for pull request
+             'base.ref',  # e.g. master
+             'base.sha'
+            ]
+    return [{x: extract_path(p, x) for x in paths} for p in pull_requests]
+
+
+def get_pull_request_commits(user, repo, pull_number):
+    url = 'https://api.github.com/repos/%s/%s/pulls/%s/commits' % (user, repo, pull_number)
+
+    r = requests.get(url)
+    if not r.ok:
+        # TODO(danvk): print error
+        sys.stderr.write('Request for %s failed.\n' % url)
+        return None
+
+    # See https://developer.github.com/v3/pulls/#list-commits-on-a-pull-request
+    commits = r.json()
+    paths = ['sha',
+            'commit.author.date',
+            'commit.message',
+            'commit.comment_count',
+            'author.login'
+            ]
+    return [{x: extract_path(p, x) for x in paths} for p in commits]
