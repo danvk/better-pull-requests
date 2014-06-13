01db37c2bd8aaf96438acd7f1f7163fbfbc56e7e
Dan Vanderkam <danvdk@gmail.com>
2014-06-13 12:34:09 -0400

    full migration to static JS for file_diff

--- static/js/file_diff.js
+++ static/js/file_diff.js
@@ -1,3 +1,4 @@
+// Render the diff using jsdifflib. Also attaches comments.
 function displayDiffs(before_ref, after_ref, baseTxt, afterTxt) {
   var diffDiv = renderDiff(baseTxt, afterTxt);
 
@@ -9,10 +10,12 @@ function displayDiffs(before_ref, after_ref, baseTxt, afterTxt) {
           comment.path == path;
   });
 
-  comments.map(
-      function(x) { return attachComment(before_ref, after_ref, diffDiv, x) });
+  comments.forEach(function(x) {
+    return attachComment(before_ref, after_ref, diffDiv, x);
+  });
 }
 
+// Creates a DOM element for the comment and attaches it appropriately.
 function attachComment(before_ref, after_ref, diffDiv, comment) {
   var pos = parseDiffPosition(comment.diff_hunk, comment.position_in_diff_hunk);
   if (comment.original_commit_id == before_ref) {
@@ -25,6 +28,7 @@ function attachComment(before_ref, after_ref, diffDiv, comment) {
   $(lineEl).append(renderComment(comment));
 }
 
+// Adds a new, editable comment box to the given position.
 function appendCommentBox(onLeft, lineNumber) {
   var $box = $(createCommentBox())
       .data({
@@ -37,107 +41,123 @@ function appendCommentBox(onLeft, lineNumber) {
   return $box;
 }
 
+// Keyboard shortcuts:
+// j/k = next/prev file
+// n/p = next/prev diff
+// u = up to pull request
+function handleKeyPress(e) {
+  if (e.ctrlKey || e.altKey || e.metaKey ||
+      e.target.tagName.toLowerCase() == 'input' ||
+      e.target.tagName.toLowerCase() == 'textarea') {
+    return;
+  }
+  if (e.keyCode == 74 || e.keyCode == 75) {  // j/k
+    var klass = (e.keyCode == 74 ? 'next' : 'prev');
+    // Any better way to visit links?
+    var url = $('a.' + klass).attr('href');
+    if (url) {
+      window.location = url;
+    }
+  } else if (e.keyCode == 78) { // n
+    // next comment
+  } else if (e.keyCode == 80) { // p
+    // previous comment
+  } else if (e.keyCode == 85) { // u
+    window.location = pull_request_url;
+  }
+  // console.log(e.keyCode);
+}
+
+// User double-clicked a line in the diff. Open a draft comment there.
+function handleDblClick(e) {
+  var $target = $(e.target);
+  var td = this;
+
+  if (this.tagName == 'TH') {
+    td = $(this).next('td').get(0);
+    $target = $(td);
+  }
+
+  // Click has to be on the code, not another comment.
+  if ($target.closest('.inline-comment').length > 0) return;
+
+  // Should only have one draft at a time.
+  var $textarea = $target.closest('td').find('textarea');
+  if ($textarea.length > 0) {
+    $textarea.focus();
+    e.preventDefault();
+    return;
+  }
+
+  var onLeft = $target.is('.before');
+  var lineClasses = td.classList;
+  var lineNumber;
+  for (var i = 0; i < lineClasses.length; i++) {
+    var m = /line-([0-9]+)/.exec(lineClasses[i]);
+    if (m) {
+      lineNumber = m[1];
+      break;
+    }
+  }
+  if (lineNumber === undefined) return;
+  var $box = appendCommentBox(onLeft, lineNumber);
+  e.preventDefault();
+  $box.find('textarea').focus();
+  return false;
+}
+
+// User clicked the "Save" button on a draft comment.
+function handleSaveComment(e) {
+  var $comment = $(this).parent('.inline-editable-comment');
+  var lineNumber = $comment.data('lineNumber');
+  var commitId = $comment.data('onLeft') ? sha1 : sha2;
+  var inReplyTo = $comment.data('inReplyTo');
+  var body = $comment.find('textarea').val();
+
+  $.ajax({
+    dataType: 'json',
+    type: 'POST',
+    url: '/save_draft',
+    data: {
+      owner: owner,
+      repo: repo,
+      pull_number: pr_number,
+      path: path,
+      commit_id: commitId,
+      line_number: lineNumber,
+      in_reply_to: inReplyTo,
+      body: body,
+    }
+  }).success(function(comment) {
+    $comment.remove();
+    diff_comments.push(comment);
+    attachComment(sha1, sha2, $('#thediff').get(0), comment);
+    // Really we should recompute addressed/unaddressed here.
+    if (inReplyTo) {
+      $('.inline-comment')
+        .filter(function(_, x) { return $(x).data('id') == inReplyTo; })
+        .addClass('addressed')
+        .removeClass('unaddressed');
+    }
+  }).error(function(err) {
+    console.error(err);
+    alert("Unable to post comment!\n\n" + JSON.stringify(err));
+  });
+}
+
 function attachHandlers() {
   $('#show-diff').on('click', function() {
     $(this).closest('form').submit();
   });
 
-  $(document).on('keydown', function(e) {
-    if (e.ctrlKey || e.altKey || e.metaKey ||
-        e.target.tagName.toLowerCase() == 'input' ||
-        e.target.tagName.toLowerCase() == 'textarea') {
-      return;
-    }
-    if (e.keyCode == 74 || e.keyCode == 75) {  // j/k
-      var klass = (e.keyCode == 74 ? 'next' : 'prev');
-      // Any better way to visit links?
-      var url = $('a.' + klass).attr('href');
-      if (url) {
-        window.location = url;
-      }
-    } else if (e.keyCode == 78) { // n
-      // next comment
-    } else if (e.keyCode == 80) { // p
-      // previous comment
-    } else if (e.keyCode == 85) { // u
-      window.location = pull_request_url;
-    }
-    // console.log(e.keyCode);
-  });
-
-  $('.diff td, .diff th').on('dblclick', function(e) {
-      var $target = $(e.target);
-      var td = this;
-
-      if (this.tagName == 'TH') {
-        td = $(this).next('td').get(0);
-        $target = $(td);
-      }
-
-      // Click has to be on the code, not another comment.
-      if ($target.closest('.inline-comment').length > 0) return;
-
-      // Should only have one draft at a time.
-      var $textarea = $target.closest('td').find('textarea');
-      if ($textarea.length > 0) {
-        $textarea.focus();
-        e.preventDefault();
-        return;
-      }
-
-      var onLeft = $target.is('.before');
-      var lineClasses = td.classList;
-      var lineNumber;
-      for (var i = 0; i < lineClasses.length; i++) {
-        var m = /line-([0-9]+)/.exec(lineClasses[i]);
-        if (m) {
-          lineNumber = m[1];
-          break;
-        }
-      }
-      if (lineNumber === undefined) return;
-      var $box = appendCommentBox(onLeft, lineNumber);
-      e.preventDefault();
-      $box.find('textarea').focus();
-      return false;
-  });
-
-  $(document).on('click', '.save-comment', function(e) {
-      var $comment = $(this).parent('.inline-editable-comment');
-      var lineNumber = $comment.data('lineNumber');
-      var commitId = $comment.data('onLeft') ? sha1 : sha2;
-      var inReplyTo = $comment.data('inReplyTo');
-      var body = $comment.find('textarea').val();
-
-      $.ajax({
-        dataType: 'json',
-        type: 'POST',
-        url: '/save_draft',
-        data: {
-          owner: owner,
-          repo: repo,
-          pull_number: pr_number,
-          path: path,
-          commit_id: commitId,
-          line_number: lineNumber,
-          in_reply_to: inReplyTo,
-          body: body,
-        }
-      }).success(function(comment) {
-        $comment.remove();
-        diff_comments.push(comment);
-        attachComment(sha1, sha2, $('#thediff').get(0), comment);
-        // Really we should recompute addressed/unaddressed here.
-        if (inReplyTo) {
-          $('.inline-comment').filter(function(_, x) { return $(x).data('id')==inReplyTo}).addClass('addressed').removeClass('unaddressed');
-        }
-      }).error(function(err) {
-        console.error(err);
-        alert("Unable to post comment!\n\n" + JSON.stringify(err));
-      });
-  }).on('click', '.discard-comment', function(e) {
-      // TODO(danvk): save draft comment at this location.
+  $(document)
+    .on('keydown', handleKeyPress)
+    .on('dblclick', '.diff td, .diff th', handleDblClick)
+    .on('click', '.save-comment', handleSaveComment)
+    .on('click', '.discard-comment', function(e) {
+      // TODO(danvk): save draft comment at this location in case the
+      // "discard" was an accident.
       var $comment = $(this).parent('.inline-editable-comment');
       $comment.remove();
-  });
+    });
 }
